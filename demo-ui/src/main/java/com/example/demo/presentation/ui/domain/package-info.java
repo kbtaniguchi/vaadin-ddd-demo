16ab2edbf5ede8483bc5a743a22ef6cb70edbefb
bububu10 <t.kiminobu@gmail.com>
2017-10-02 01:57:17 +0900

    revise package-info

--- demo-ui/src/main/java/com/example/demo/presentation/ui/domain/package-info.java
+++ demo-ui/src/main/java/com/example/demo/presentation/ui/domain/package-info.java
@@ -1,7 +1,7 @@
 /**
  * domain specific ui & logic (mvp pattern)
  * view: only ui concern
- * presenter: without depending on ui component
+ * presenter: without depending on ui component, responsible to event handling
  * model: domain view model
  */
 package com.example.demo.presentation.ui.domain;
