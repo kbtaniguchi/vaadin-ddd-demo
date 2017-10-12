ad31a38db40e0b64629895f0bf38f46141020bec
bububu10 <t.kiminobu@gmail.com>
2017-10-12 22:27:07 +0900

    test selenide

---
+++ demo-ui/src/test/java/com/example/demo/presentation/view/useradmin/root/view/UserAdminViewTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.presentation.view.useradmin.root.view;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.example.demo.IntegrationTestApplication;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.codeborne.selenide.Selenide.open;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = IntegrationTestApplication.class)
+@ActiveProfiles("test")
+public class UserAdminViewTest {
+    @LocalServerPort
+    private int port;
+
+    @BeforeClass
+    public static void setUp() {
+        Configuration.browser = WebDriverRunner.CHROME;
+        System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver");
+    }
+
+
+    @Test
+    public void test() {
+        open("http://localhost:" + port);
+    }
+}
