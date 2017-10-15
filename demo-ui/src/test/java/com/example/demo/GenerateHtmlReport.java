304c5731e47cb6154387d29f418d52c357f1cb27
bububu10 <t.kiminobu@gmail.com>
2017-10-15 17:36:47 +0900

    とりあえずhtmlレポートを出せるようにした

---
+++ demo-ui/src/test/java/com/example/demo/GenerateHtmlReport.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import com.example.demo.presentation.scenario.ScenarioTestUtils;
+
+public class GenerateHtmlReport {
+    public static void main(String[] args) {
+        ScenarioTestUtils.generateHtmlReports();
+    }
+}
